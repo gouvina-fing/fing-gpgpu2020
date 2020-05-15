f65b0c50bfb743a45d3ee0b084e667d0e5cca7d5
gouvina <germanouvina@gmail.com>
2020-05-15 01:06:09 -0300

    Ejercicio 1: Estructura, parte A

---
+++ practico4/ej1_main.cpp
@@ -0,0 +1,27 @@
+#include "util.h"
+#include "CImg.h"
+
+using namespace cimg_library;
+
+void transpose_global(float * image, int width, int height, float * image_out);
+    
+int main(int argc, char** argv){
+
+	const char * path;
+
+	if (argc < 2) printf("Invocar como: './ej1.x nombre_archivo'.\n");
+    else
+        path = argv[1];
+
+	CImg<float> image(path);
+	CImg<float> image_out(image.height(), image.width(),1,1,0);
+
+	float *img_matrix = image.data();
+    float *img_out_matrix = image_out.data();
+   	
+	transpose_global(img_matrix, image.width(), image.height(), img_out_matrix);
+   	image_out.save("output_transpose_global.ppm");
+    
+	return 0;
+}
+
---
+++ practico4/transpose.cu
@@ -0,0 +1,79 @@
+#include "util.h"
+
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <algorithm>    // std::min std::max
+
+using namespace std;
+
+// Ej 1a) Kernel 
+__global__ void transpose_global_kernel(float* d_input, int width, int height, float* d_output) {
+    int imgx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    int imgy = (blockIdx.y * blockDim.y) + threadIdx.y;
+    if (imgx < width && imgy < height) {
+        d_output[(imgx*height) + imgy] = d_input[(imgy*width) + imgx];
+        // d_output[(imgy*width) + imgx]
+    }
+}
+
+// Ej 1) A
+void transpose_global(float * img_in, int width, int height, float * img_out){
+    
+    // Auxiliar para contar tiempo total
+    float t_total = 0;
+    
+    // Etapa 1: Reserva de Memoria
+    CLK_CUEVTS_INIT;
+    CLK_CUEVTS_START;
+    // Reserva en CPU
+    unsigned int size = width * height * sizeof(float);
+    float * device_img_in = (float *)malloc(size);
+    float * device_img_out = (float *)malloc(size);
+    // Reserva en GPU
+    CUDA_CHK(cudaMalloc((void**)& device_img_in, size));
+    CUDA_CHK(cudaMalloc((void**)& device_img_out, size));
+    CLK_CUEVTS_STOP;
+    CLK_CUEVTS_ELAPSED;
+    printf("Tiempo transposicion global (Reserva de memoria): %f ms\n", t_elap);
+    t_total = t_total + t_elap;
+    
+    // Etapa 2: Transferencia de datos (Host -> Device)
+    CLK_CUEVTS_START;
+    CUDA_CHK(cudaMemcpy(device_img_in, img_in, size, cudaMemcpyHostToDevice)); // puntero destino, puntero origen, numero de bytes a copiar, tipo de transferencia
+    CLK_CUEVTS_STOP;
+    CLK_CUEVTS_ELAPSED;
+    printf("Tiempo transposicion global (Transferencia de datos (Host -> Device)): %f ms\n", t_elap);
+    t_total = t_total + t_elap;
+
+    // Etapa 3: Definir grilla
+    int block_size = 32; // TODO: Definir constante
+    int block_amount_x = width / block_size + (width % block_size != 0); // Division with ceiling
+    int block_amount_y = height / block_size + (height % block_size != 0); // Division with ceiling
+    dim3 tamGrid(block_amount_x, block_amount_y); // Grid dimension
+    dim3 tamBlock(block_size, block_size); // Block dimension
+
+    // Etapa 4 : Lanzar Kernel
+    CLK_CUEVTS_START;
+    transpose_global_kernel<<<tamGrid, tamBlock>>>(device_img_in, width, height, device_img_out);
+    // Sincronizar threads antes de parar timers
+    cudaDeviceSynchronize(); 
+    CLK_CUEVTS_STOP;
+    CLK_CUEVTS_ELAPSED;
+    printf("Tiempo transposicion global (Kernel): %f ms\n", t_elap);
+    t_total = t_total + t_elap;
+
+    // Etapa 5: Transferencia de Datos (Device -> Host)
+    CLK_CUEVTS_START;
+    CUDA_CHK(cudaMemcpy(img_out, device_img_out, size, cudaMemcpyDeviceToHost)); // puntero destino, puntero origen, numero de bytes a copiar, tipo de transferencia
+    CLK_CUEVTS_STOP;
+    CLK_CUEVTS_ELAPSED;
+    printf("Tiempo transposicion global (Transferencia de datos (Host <- Device)): %f ms\n", t_elap);
+    t_total = t_total + t_elap;
+    printf("Tiempo transposicion global: %f ms\n", t_total);
+    printf("\n");
+
+    // Etapa 6: Liberación de Memoria
+    CUDA_CHK(cudaFree(device_img_in));
+    CUDA_CHK(cudaFree(device_img_out));
+}
